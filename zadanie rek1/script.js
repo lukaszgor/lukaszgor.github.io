d6fb8ae3b348bf1765463683a96db32420f8926c
Łukasz Górski <lukasz.gg13@gmail.com>
2017-09-20 13:28:16 +0200

    pk
    pk

--- zadanie rek1/script.js
+++ zadanie rek1/script.js
@@ -1,14 +1,36 @@
 'use strict';
-var time = 20; //jak długo
+//var time = 20; //jak długo
+//var initialOffset = '440';
+//var i = 1 
+//var interval = setInterval(function() {
+//    $('.circle_animation').css('stroke-dashoffset', initialOffset-(i*(initialOffset/time)));
+//    $('h2').text(i+" sek").css({color:"white"});
+//    
+//    if (i == time) {
+//        clearInterval(interval);
+//        $("h2").hide("fast");
+//    }
+//    ++i;  
+//}, 1000);
+/////////////////////////////////////////////cos zmajstrowane 
+ var time = 20;
 var initialOffset = '440';
-var i = 1 
-var interval = setInterval(function() {
+var i=1;
+
+var interval = setInterval(function(){ 
     $('.circle_animation').css('stroke-dashoffset', initialOffset-(i*(initialOffset/time)));
     $('h2').text(i+" sek").css({color:"white"});
-    
-    if (i == time) {
-        clearInterval(interval);
-        $("h2").hide("fast");
-    }
-    ++i;  
-}, 1000);
+    twojTimer() }, 1000);
+
+function twojTimer() {
+  
+  if (time > 0) {
+    time -= 1;
+//    document.getElementById('timer').innerHTML = time;
+  }    else {
+    clearInterval(interval);
+      $("h2").hide("fast");
+  }++i;
+   
+ $('h2').text(time+" sek").css({color:"white"});
+}
