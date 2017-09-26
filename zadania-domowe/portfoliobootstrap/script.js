4b0108b088a0c17cd53829ef2cd720059bf10682
Łukasz Górski <lukasz.gg13@gmail.com>
2017-09-26 18:03:13 +0200

    skonczone tylko co z funkcja
    skonczone tylko co z funkcja

--- zadania-domowe/portfoliobootstrap/script.js
+++ zadania-domowe/portfoliobootstrap/script.js
@@ -1,1 +1,10 @@
 'use strict';
+$(document).ready(function() {
+    
+    smoothScroll();
+});
+function smoothScroll() {
+    $('#main a[href*="#"]:not([href="#"])').click(function(){
+		$('body').animate( { scrollTop: $(this.hash).offset().top - 50 }, 1000 );
+	});
+}
