2608ffa1b9f568ed83e0314697650792282bd30d
Łukasz Górski <lukasz.gg13@gmail.com>
2017-10-08 19:36:21 +0200

    dodanie portfolio
    dodanie portfolio

--- zadania-domowe/kursBitcoina/main.js
+++ zadania-domowe/kursBitcoina/main.js
@@ -1,196 +1,162 @@
 'use strict';
-$( document ).ready( function() {
-    var oldValueBuy = 0;
-    var oldValueSell = 0;
-   
-  
-    
-function no(){
-    
-$.getJSON('https://blockchain.info/pl/ticker', function (date) {
-// wyswietl w konsoli
-console.log(date);
-   
-    
-   $("#kurs1").text(date.PLN.buy);    
-   $("#kurs2").text( date.PLN.sell); 
-//    
-    oldValueBuy=date.PLN.buy;
-    oldValueSell=date.PLN.sell;
-//    
-    console.log("Zmiana buy: " + (date.PLN.buy-oldValueBuy))
-    console.log("Zmiana sell: " + (date.PLN.sell-oldValueSell))
- 
-    
-    
-//   
-//    
-//    if (jeden>0){
-//        $(".ws").text("wzrost");
-//        console.log("wzrost");
-//    }
-//    if(jeden<0){
-//        $(".ws").text("spadek");
-//         console.log("spadek");
-//    }
-//    if(jeden=0){
-//        $(".ws").text("stale");
-//        consol.log("rowne");
-//    }
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    if (dwa>0){
-//        $(".ws").text("wzrost");
-//        console.log("wzrost");
-//    }
-//    if(dwa<0){
-//        $(".ws").text("spadek");
-//         console.log("spadek");
-//    }
-//    if(dwa=0){
-//        $(".ws").text("stale");
-//        consol.log("rowne");
-//    }
-    
-    
-//    
-  
-    
-} );
+var oldValueBuy = 0;
+var oldValueSell = 0;
+
+$(document).ready(function () {
+
+
+
+
+    function no() {
+
+        $.getJSON('https://blockchain.info/pl/ticker', function (date) {
+            // wyswietl w konsoli
+            console.log(date);
+
+
+
+            //    
+            oldValueBuy = parseInt(date.PLN.buy);
+            oldValueSell = parseInt(date.PLN.sell);
+            
+            $("#kurs1").text(oldValueBuy);
+            $("#kurs2").text(oldValueSell);            
+            
+            //    
+            console.log("Zmiana buy: " + (parseInt(date.PLN.buy) - oldValueBuy))
+            console.log("Zmiana sell: " + (date.PLN.sell - oldValueSell))
+
+
+        });
     }
-    no();
-   
-window.setInterval(no,5000);
-    
-    
-    
-    
-    
 
 
-   
+    window.setInterval(no, 5000);
+
+
+
+
+
+
+
+
     ////////////////////////////////////////////////////5sekund
-    function sek5(){
-     
-    $( "jeden" ).click( function() {
-    
-    
-$.getJSON('https://blockchain.info/pl/ticker', function (data) {
-// wyswietl w konsoli
-console.log(data);
-    
-      
-   $("#kurs1").text("Kupno: "+ date.PLN.buy);    
-   $("#kurs2").text("Sprzedaż: "+ date.PLN.sell);  
-    
-       
-    
-} );
-                
-} );
-    }
-                       
-                       
-    sek5()
-        window.setInterval(sek5,5000);
-    //////////////////////////////////////////////////10sekund
-        
-         function sek10(){
-     
-    $( "dwa" ).click( function() {
-     window.clearInterval()
-    
-$.getJSON('https://blockchain.info/pl/ticker', function (data) {
-// wyswietl w konsoli
-console.log(data);
-    
-      
-   $("#kurs1").text("Kupno: "+ date.PLN.buy);    
-   $("#kurs2").text("Sprzedaż: "+ date.PLN.sell);  
-    
-       
-    
-} );
-                
-} );
-    }
-                       
-                       
-    sek10()
-        window.setInterval(sek10,10000);
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        /////////////////////////////////////////////////////30sekund
-    
-         
-         function sek30(){
-     
-    $( "trzy" ).click( function() {
-    window.clearInterval()
-    
-$.getJSON('https://blockchain.info/pl/ticker', function (data) {
-// wyswietl w konsoli
-console.log(data);
-    
-      
-   $("#kurs1").text("Kupno: "+ date.PLN.buy);    
-   $("#kurs2").text("Sprzedaż: "+ date.PLN.sell);  
-    
-       
-    
-} );
-                
-} );
-    }
-                       
-                       
-    sek30()
-        window.setInterval(sek30,30000);
-        
-        
-    
-    
-    
-    
-    /////////////////////////////////////////////////////60sekund
+//    function sek5() {
+//
+//        $("jeden").click(function () {
+//
+//
+//            $.getJSON('https://blockchain.info/pl/ticker', function (data) {
+//                // wyswietl w konsoli
+//                console.log(data);
+//
+//
+//                $("#kurs1").text("Kupno: " + date.PLN.buy);
+//                $("#kurs2").text("Sprzedaż: " + date.PLN.sell);
+//
+//
+//
+//            });
+//
+//        });
+//    }
 
-    
-       function sek60(){
-     
-    $( "trzy" ).click( function() {
-    window.clearInterval()
-    
-$.getJSON('https://blockchain.info/pl/ticker', function (data) {
-// wyswietl w konsoli
-console.log(data);
-    
-      
-   $("#kurs1").text("Kupno: "+ date.PLN.buy);    
-   $("#kurs2").text("Sprzedaż: "+ date.PLN.sell);  
-    
-       
-    
-} );
-                
-} );
-    }
-                       
-                       
-    sek60()
-        window.setInterval(sek60,60000);
-        
-  
-    
+//
+//    sek5()
+//    window.setInterval(sek5, 5000);
+//    //////////////////////////////////////////////////10sekund
+//
+//    function sek10() {
+//
+//        $("dwa").click(function () {
+//            window.clearInterval()
+//
+//            $.getJSON('https://blockchain.info/pl/ticker', function (data) {
+//                // wyswietl w konsoli
+//                console.log(data);
+//
+//
+//                $("#kurs1").text("Kupno: " + date.PLN.buy);
+//                $("#kurs2").text("Sprzedaż: " + date.PLN.sell);
+//
+//
+//
+//            });
+//
+//        });
+//    }
+//
+//
+//    sek10()
+//    window.setInterval(sek10, 10000);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//    /////////////////////////////////////////////////////30sekund
+//
+//
+//    function sek30() {
+//
+//        $("trzy").click(function () {
+//            window.clearInterval()
+//
+//            $.getJSON('https://blockchain.info/pl/ticker', function (data) {
+//                // wyswietl w konsoli
+//                console.log(data);
+//
+//
+//                $("#kurs1").text("Kupno: " + date.PLN.buy);
+//                $("#kurs2").text("Sprzedaż: " + date.PLN.sell);
+//
+//
+//
+//            });
+//
+//        });
+//    }
+//
+//
+//    sek30()
+//    window.setInterval(sek30, 30000);
+//
+//
+//
+//
+//
+//
+//    /////////////////////////////////////////////////////60sekund
+//
+//
+//    function sek60() {
+//
+//        $("trzy").click(function () {
+//            window.clearInterval()
+//
+//            $.getJSON('https://blockchain.info/pl/ticker', function (data) {
+//                // wyswietl w konsoli
+//                console.log(data);
+//
+//
+//                $("#kurs1").text("Kupno: " + date.PLN.buy);
+//                $("#kurs2").text("Sprzedaż: " + date.PLN.sell);
+//
+//
+//
+//            });
+//
+//        });
+//    }
+//
+//
+//    sek60()
+//    window.setInterval(sek60, 60000);
+
+
+
 });
